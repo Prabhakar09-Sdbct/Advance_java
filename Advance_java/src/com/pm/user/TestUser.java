c0d725193d02f851351c5bacef8e819aef9b3350
prabhakar mandloi <prabhakarmandloi@gmail.com>
2024-09-14 13:48:12 +0530

    Added search

--- Advance_java/src/com/pm/user/TestUser.java
+++ Advance_java/src/com/pm/user/TestUser.java
@@ -1,100 +1,113 @@
 package com.pm.user;
 
-import java.util.Scanner;
+import java.util.Iterator;
+import java.util.List;
 
 public class TestUser {
 
 	public static void main(String[] args) throws Exception {
-
-		Scanner scanner = new Scanner(System.in);
-
-		while (true) {
-			System.out.println("Please select an option:");
-			System.out.println("1. Add");
-			System.out.println("2. Update");
-			System.out.println("3. Delete");
-			System.out.println("4. Exit");
-			System.out.print("Enter your choice (1-4): ");
-			int choice = scanner.nextInt();
-
-			switch (choice) {
-			case 1:
-				testAdd(scanner);
-				break;
-			case 2:
-				testUpdate(scanner);
-				break;
-			case 3:
-				testDelete(scanner);
-				break;
-			case 4:
-				System.out.println("Exiting the program.");
-				scanner.close();
-				System.exit(0);
-			default:
-				System.out.println("Invalid choice. Please select a valid option.");
-			}
-		}
+                testAdd();
+//				testDelete();
+//				testFindByPk();
+//				testFindByLoginId();
+//      		search();
 	}
 
-	public static void testAdd(Scanner scanner) throws Exception {
+	public static void testAdd() throws Exception {
 		UserBean bean = new UserBean();
 
-		System.out.print("Enter the id: ");
-		bean.setId(scanner.nextInt());
-		scanner.nextLine();
-
-		System.out.print("Enter the first Name: ");
-		bean.setFirstName(scanner.nextLine());
-
-		System.out.print("Enter the last Name: ");
-		bean.setLastName(scanner.nextLine());
-
-		System.out.print("Enter the login Id: ");
-		bean.setLoginId(scanner.nextLine());
-
-		System.out.print("Enter the password: ");
-		bean.setPassword(scanner.nextLine());
-
-		System.out.print("Enter the Address: ");
-		bean.setAddress(scanner.nextLine());
+		bean.setFirstName("prabhakar6");
+		bean.setLastName("mandloi");
+		bean.setLoginId("mandloi@gmail.com");
+		bean.setPassword("1234");
+		bean.setAddress("indore");
 
 		UserModal modal = new UserModal();
 		modal.add(bean);
+
+		System.out.println("=============================");
 	}
 
-	public static void testUpdate(Scanner scanner) throws Exception {
+	public static void testUpdate() throws Exception {
 		UserBean bean = new UserBean();
 
-		System.out.print("Enter the id: ");
-		bean.setId(scanner.nextInt());
-		scanner.nextLine();
-
-		System.out.print("Enter the first Name: ");
-		bean.setFirstName(scanner.nextLine());
-
-		System.out.print("Enter the last Name: ");
-		bean.setLastName(scanner.nextLine());
-
-		System.out.print("Enter the login Id: ");
-		bean.setLoginId(scanner.nextLine());
-
-		System.out.print("Enter the password: ");
-		bean.setPassword(scanner.nextLine());
-
-		System.out.print("Enter the Address: ");
-		bean.setAddress(scanner.nextLine());
+		bean.setId(2);
+		bean.setFirstName("prabhakaran");
+		bean.setLastName("mandloi");
+		bean.setLoginId("mandloi@gmail.com");
+		bean.setPassword("1234");
+		bean.setAddress("indore");
 
 		UserModal modal = new UserModal();
 		modal.update(bean);
+
+		System.out.println("=============================");
 	}
 
-	public static void testDelete(Scanner scanner) throws Exception {
-		System.out.print("Enter the id: ");
-		scanner.nextLine();
-		
+	public static void testDelete() throws Exception {
+
 		UserModal modal = new UserModal();
-		modal.delete(scanner.nextInt());
+		modal.delete(2);
+
+		System.out.println("=============================");
 	}
 
+	public static void testFindByPk() throws Exception {
+		UserModal modal = new UserModal();
+		UserBean bean = modal.findByPk(2);
+
+		if (bean != null) {
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getFirstName());
+			System.out.print("\t" + bean.getLastName());
+			System.out.print("\t" + bean.getLoginId());
+			System.out.print("\t" + bean.getPassword());
+			System.out.println("\t" + bean.getAddress());
+		} else {
+			System.out.println("user not found");
+		}
+
+		System.out.println("=============================");
+	}
+	
+	public static void testFindByLoginId() throws Exception {
+		UserModal modal = new UserModal();
+		UserBean bean = modal.findByPk(2);
+
+		if (bean != null) {
+			System.out.print(bean.getId());
+			System.out.print("\t" + bean.getFirstName());
+			System.out.print("\t" + bean.getLastName());
+			System.out.print("\t" + bean.getLoginId());
+			System.out.print("\t" + bean.getPassword());
+			System.out.println("\t" + bean.getAddress());
+		} else {
+			System.out.println("user not found");
+		}
+
+		System.out.println("=============================");
+	}
+	
+	public static void search() throws Exception {
+		UserBean bean = new UserBean();
+		bean.setFirstName("prabhakar");
+		bean.setLastName("mandloi");
+
+		UserModal modal = new UserModal();
+		List list = modal.search(bean, 1, 5);
+
+		Iterator it = list.iterator();
+		while (it.hasNext()) {
+			bean = (UserBean) it.next();
+			if (bean != null) {
+				System.out.print(bean.getId());
+				System.out.print("\t" + bean.getFirstName());
+				System.out.print("\t" + bean.getLastName());
+				System.out.print("\t" + bean.getLoginId());
+				System.out.print("\t" + bean.getPassword());
+				System.out.println("\t" + bean.getAddress());
+			}
+		}
+		System.out.println("=============================");
+	}
 }
--- Advance_java/src/com/pm/user/UserModal.java
+++ Advance_java/src/com/pm/user/UserModal.java
@@ -3,52 +3,149 @@ package com.pm.user;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserModal {
 	
+	public Integer nextPk() throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+		PreparedStatement psmt = con.prepareStatement("select max(id) from user");
+		
+		int pk = 0;
+		ResultSet rs = psmt.executeQuery();
+		
+		while(rs.next()) {
+			pk = rs.getInt(1);
+		}
+		return pk + 1;
+	}
+
 	public void add(UserBean bean) throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
 		PreparedStatement psmt = con.prepareStatement("insert into user values(?,?,?,?,?,?)");
-		
-		psmt.setInt(1, bean.getId());
+
+		psmt.setInt(1, nextPk());
 		psmt.setString(2, bean.getFirstName());
 		psmt.setString(3, bean.getLastName());
 		psmt.setString(4, bean.getLoginId());
 		psmt.setString(5, bean.getPassword());
 		psmt.setString(6, bean.getAddress());
-		
-	   int i = psmt.executeUpdate();
-	   System.out.println(" Data inserted : "+i);
+
+		int i = psmt.executeUpdate();
+		System.out.println(" Data inserted : " + i);
 	}
-	
+
 	public void update(UserBean bean) throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
-		PreparedStatement psmt =  con.prepareStatement("update user set first_name = ?, last_name = ?, login_id = ?, password = ?, address = ? where id = ?");
-		
+		PreparedStatement psmt = con.prepareStatement(
+				"update user set first_name = ?, last_name = ?, login_id = ?, password = ?, address = ? where id = ?");
+
 		psmt.setString(1, bean.getFirstName());
 		psmt.setString(2, bean.getLastName());
 		psmt.setString(3, bean.getLoginId());
 		psmt.setString(4, bean.getPassword());
 		psmt.setString(5, bean.getAddress());
 		psmt.setInt(6, bean.getId());
-		
-	   int i = psmt.executeUpdate();
-	   System.out.println(" Data updated : "+i);
-	
+
+		int i = psmt.executeUpdate();
+		System.out.println(" Data updated : " + i);
+
 	}
-	
+
 	public void delete(int id) throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
-		PreparedStatement psmt =  con.prepareStatement("delete from user where id = ?");
-	
+		PreparedStatement psmt = con.prepareStatement("delete from user where id = ?");
+
 		psmt.setInt(1, id);
+
+		int i = psmt.executeUpdate();
+		System.out.println(" Data deleted : " + i);
+
+	}
+
+	public UserBean findByPk(int id) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+		PreparedStatement psmt = con.prepareStatement("select * from user where id = ?");
+        psmt.setInt(1, id);
 		
-	   int i = psmt.executeUpdate();
-	   System.out.println(" Data deleted : "+i);
+        UserBean bean = null;
+		ResultSet rs = psmt.executeQuery();
+		while (rs.next()) {
+			bean = new UserBean();
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setAddress(rs.getString(6));
+		}
+		return bean;
+	}
 	
+	public UserBean findByLoginId(int loginId) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+		PreparedStatement psmt = con.prepareStatement("select * from user where login_id = ?");
+        psmt.setInt(1, loginId);
+		
+        UserBean bean = null;
+		ResultSet rs = psmt.executeQuery();
+		while (rs.next()) {
+			bean = new UserBean();
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setAddress(rs.getString(6));
+		}
+		return bean;
+	}
+	
+	public List search(UserBean bean, int pageNo, int pageSize) throws Exception {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "root", "root");
+		StringBuffer sql = new StringBuffer("select * from user where 1=1");
+		
+		if(bean != null) {
+		   if(bean.getFirstName() != null && bean.getFirstName().length() > 0) {
+			   sql.append(" and first_name like '"+bean.getFirstName() +"%'");
+		   } 
+		   
+		   if(bean.getLastName() != null && bean.getLastName().length() > 0) {
+			   sql.append(" and last_name like '"+bean.getLastName() +"%'");
+		   }
+		}
+		
+		if(pageSize > 0) {
+			pageNo = (pageNo - 1) * pageSize;
+			sql.append(" limit "+ pageNo +"," + pageSize);
+		}
+		
+		PreparedStatement psmt = con.prepareStatement(sql.toString());
+		
+		List list = new ArrayList();
+		ResultSet rs = psmt.executeQuery();
+		while (rs.next()) {
+			bean = new UserBean();
+			bean.setId(rs.getInt(1));
+			bean.setFirstName(rs.getString(2));
+			bean.setLastName(rs.getString(3));
+			bean.setLoginId(rs.getString(4));
+			bean.setPassword(rs.getString(5));
+			bean.setAddress(rs.getString(6));
+			list.add(bean);
+		}
+		
+		
+		return list;
 	}
 
 }
